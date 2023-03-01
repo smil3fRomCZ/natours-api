56ba2e2f4d670b83105fda3cbbb9791b8ba7ecfd
smil3fRomCZ <janmelicherik84@gmail.com>
2023-03-01 16:00:18 +0100

    created global error handler

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // eslint-disable-next-line import/no-extraneous-dependencies, node/no-unpublished-require
 const morgan = require('morgan');
+const AppError = require('./utils/appErrorHandler');
+const globalErrorHandler = require('./controllers/errorController');
 
 const userRouter = require('./routes/userRouter');
 const tourRouter = require('./routes/tourRouter');
@@ -17,5 +19,11 @@ app.use(express.static(`${__dirname}/public`));
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Cant find ${req.originalUrl}`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
