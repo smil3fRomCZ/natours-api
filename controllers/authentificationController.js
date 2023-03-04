ee3594e04176ce1a991d563a8c1f5220bb47f961
smil3fRomCZ <janmelicherik84@gmail.com>
2023-03-04 14:10:13 +0100

    added token during user registration

--- controllers/authentificationController.js
+++ controllers/authentificationController.js
@@ -1,12 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/asyncHandler');
 
 exports.signUp = catchAsync(async (req, res, next) => {
-  const registrationData = req.body;
+  const registrationData = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+  };
   const newUser = await User.create(registrationData);
+  const secretKey = process.env.JWT_SECRET;
+  const expiration = process.env.JWT_EXPIRES_IN;
+  const token = jwt.sign({ id: newUser._id }, secretKey, {
+    expiresIn: expiration,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
